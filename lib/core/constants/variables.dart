93219554a083ee7fd6f0753047a66df45d667b02
genzprogrammer <bibitraikhanazzaki@gmail.com>
2026-01-14 13:55:02 +0700

    update

--- lib/core/constants/variables.dart
+++ lib/core/constants/variables.dart
@@ -1,5 +1,5 @@
 class Variables {
   static const String appName = 'CURUG PINANG';
   // static const String baseUrl = 'http://145.223.21.247:7151';
-  static const String baseUrl = 'http://172.20.9.67:8000';
+  static const String baseUrl = 'http://192.168.100.179:8000';
 }
